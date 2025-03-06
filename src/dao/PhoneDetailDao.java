48aac176409e4393d970d6eeb425e064cda7ae77
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-03-06 09:45:27 +0900

    feat(dao) : PhoneDetailDao : PhoneDetail 수정 메서드 구현

--- src/dao/PhoneDetailDao.java
+++ src/dao/PhoneDetailDao.java
@@ -37,4 +37,31 @@ public class PhoneDetailDao {
 
         return ret;
     }
+
+    public int updatePhoneDetail(PhoneDetailDto phoneDetail) {
+        int ret = -1;
+        String query = "UPDATE phon_detail SET processor = ?, ram = ?, storage = ?, battery = ?, weight = ? WHERE phone_id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = DBManager.getConnection();
+            pstmt = conn.prepareStatement(query);
+
+            pstmt.setString(1, phoneDetail.getProcessor());
+            pstmt.setString(2, phoneDetail.getRam());
+            pstmt.setString(3, phoneDetail.getStorage());
+            pstmt.setInt(4, phoneDetail.getBattery());
+            pstmt.setInt(5, phoneDetail.getWeight());
+            pstmt.setInt(6, phoneDetail.getPhone_id());
+
+            ret = pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
 }
