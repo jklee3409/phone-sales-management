a151a451ea97a89d8f7267e8d0fc74dce4ada726
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-03-06 13:36:23 +0900

    feat(ui) : StockViewScreen : JTable 셀 수정 불가능하도록 변경

--- src/ui/StockViewScreen.java
+++ src/ui/StockViewScreen.java
@@ -3,6 +3,8 @@ package ui;
 import dao.PhoneDao;
 import dto.PhoneDto;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.util.List;
@@ -18,12 +20,31 @@ public class StockViewScreen extends JFrame {
         setLocationRelativeTo(null);
 
         String[] columnNames = {"ID", "모델명", "브랜드", "출시일", "가격", "재고"};
-        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // 셀 편집 불가
+            }
+        };
+
         JTable table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
         add(scrollPane);
 
         loadStockData(tableModel);
+
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) { // 더블 클릭 감지
+                    int selectedRow = table.getSelectedRow();
+                    if (selectedRow != -1) {
+                        int phoneId = (int) table.getValueAt(selectedRow, 0); // 첫 번째 열(phone_id) 가져오기
+                        new PhoneDetailViewScreen(phoneId);
+                    }
+                }
+            }
+        });
 
         setVisible(true);
     }
