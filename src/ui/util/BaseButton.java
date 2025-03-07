71aff7421224b4259f11945316f60e42c600fae9
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-03-07 10:21:09 +0900

    feat(util) : BaseButton : 수정 및 삭제 버튼 기본 기능 추상화

--- src/ui/util/BaseButton.java
+++ src/ui/util/BaseButton.java
@@ -6,12 +6,12 @@ import dao.PhoneDetailDao;
 import javax.swing.*;
 import java.awt.*;
 
-public abstract class PhoneBaseScreen extends JFrame {
+public abstract class BaseButton extends JFrame {
     protected PhoneFormPanel formPanel;
     protected PhoneDao phoneDao = new PhoneDao();
     protected PhoneDetailDao phoneDetailDao = new PhoneDetailDao();
 
-    public PhoneBaseScreen(String title) {
+    public BaseButton(String title) {
         setTitle(title);
         setSize(400, 450);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -23,12 +23,21 @@ public abstract class PhoneBaseScreen extends JFrame {
     }
 
     protected abstract void handleAction(); // 버튼 액션을 강제 구현하도록 설정
+    protected abstract void handleDelete(); // 삭제 버튼 액션을 강제 구현하도록 설정
 
     protected void addActionButton(String text) {
         JButton actionButton = new JButton(text);
         actionButton.addActionListener(e -> handleAction());
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(actionButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    protected void addDeleteButton(String text) {
+        JButton deleteButton = new JButton(text);
+        deleteButton.addActionListener(e -> handleDelete());
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(deleteButton);
         add(buttonPanel, BorderLayout.SOUTH);
     }
 }
