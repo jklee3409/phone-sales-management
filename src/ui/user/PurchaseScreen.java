605e8a325025f0192db96160ab4ed2a9b438188a
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-03-07 11:06:12 +0900

    feat(ui) : PurchaseScreen : 유저 구매 후 구매 화면 재고 및 잔액 업데이트

--- src/ui/user/PurchaseScreen.java
+++ src/ui/user/PurchaseScreen.java
@@ -57,8 +57,7 @@ public class PurchaseScreen extends JFrame {
                     int selectedRow = phoneTable.getSelectedRow();
                     if (selectedRow != -1) {
                         int phoneId = (int) tableModel.getValueAt(selectedRow, 0);
-                        new PhoneDetailViewScreen(phoneId, user, null,true, false); // 구매 가능 모드, 관리자 아님
-                        updateUI();
+                        new PhoneDetailViewScreen(phoneId, user, null,PurchaseScreen.this, true, false); // 구매 가능 모드, 관리자 아님
                     }
                 }
             }
